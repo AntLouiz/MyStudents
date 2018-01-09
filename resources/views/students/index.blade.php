570d5dba13ca7b8526d9192c681b09e78456b53c
AntLouiz <luizrodrigo46@hotmail.com>
2018-01-09 10:17:39 -0300

    some updates on the index style

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,18 +1,45 @@
 @extends('base')
 
 @section('content')
+  <style type="text/css">
+    .card{
+          width: 26rem;
+          border: solid #4a549c;
+          padding: 1em;
+          margin: 7px;
+    }
+    .card-img-top{
+          width: 6em;
+          border-radius: 100%;
+    }
+  </style>
   <div>
-    <ul>
-    <div class="container">
+    <div class="container center">
+      @if(session()->has('message'))
+          <div class="alert alert-success">
+              {{ session()->get('message') }}
+          </div>
+      @endif
       <div class="row">
               @foreach ($students as $student)
-                  <li>
-                    {{$student->name}}
-                    <a href="{{ route('students.update', $student) }}">Update</a>
-                  </li>
+              
+                    <div class="card col-md-2">
+                      <img class="card-img-top" src="{{ asset('images/default_avatar.png') }}" alt="Card image cap">
+                      <div class="card-body">
+                        <h5 class="card-title">{{$student->name}}</h5>
+                        <p class="card-text">
+                          Email: {{$student->email}}
+                        </p>
+                        <a 
+                          href="{{ route('students.update', $student) }}"
+                          class="btn btn-primary"
+                        >
+                          Update
+                        </a>
+                      </div>
+                    </div>
               @endforeach
-      </div>
     </div>
-    </ul>
+    </div>
   </div>
 @endsection
