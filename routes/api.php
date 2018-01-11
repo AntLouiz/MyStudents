d06f9fd3e91d8d0e4170f34a633617292d9903eb
AntLouiz <luizrodrigo46@hotmail.com>
2018-01-11 12:37:11 -0300

    new api routes added

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/notifications/count', function(){
+  return StudentNotification::where('checked', 0)->count();
+});
 
 Route::get('/notifications', function() {
-  return StudentNotification::all();
+  return StudentNotification::where('checked', 0)->get();
 });
