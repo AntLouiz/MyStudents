aecff5805ecfbcffa83513a6b3b381bb5aa72474
AntLouiz <luizrodrigo46@hotmail.com>
2018-01-14 22:29:48 -0300

    a new controller added

---
+++ app/Http/Controllers/StudentNotificationController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentNotificationController extends Controller
+{
+    public function check(Request $request){
+      return $request;
+    }
+}
