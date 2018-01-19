f33abcd23938393bb8f03b73c5414a8184b2b394
AntLouiz <luizrodrigo46@hotmail.com>
2018-01-19 13:19:28 -0300

    checking the notifications on the checked method

--- app/Http/Controllers/StudentNotificationController.php
+++ app/Http/Controllers/StudentNotificationController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\StudentNotification;
 
 class StudentNotificationController extends Controller
 {
     public function check(Request $request){
-      return $request;
+      foreach ($request->all() as $notification) {
+        $notification_id = $notification['id'];
+        StudentNotification::where('id', $notification_id)
+        ->update(['checked' => 1]);
+      }
+      return StudentNotification::orderBy('id', 'DESC')->get();;
     }
 }
