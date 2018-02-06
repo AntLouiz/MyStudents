20d0d77d18df8f26217c8af821b68706a27f79f2
AntLouiz <luizrodrigo46@hotmail.com>
2018-02-05 23:11:23 -0300

    index page added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,13 @@
 @extends('base')
 
 @section('content')
+
+<div class="container">
+  <div class="row">
+    <h1>
+      Welcome to my first Laravel project
+    </h1>
+  </div>
+</div>
+
 @endsection
